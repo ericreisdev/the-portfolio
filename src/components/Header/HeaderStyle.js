3c057c43093d0353262c23e72d3db54f16a43ed8
ericreisdev <ericreis.dev@gmail.com>
2024-02-20 18:58:37 -0300

    lacking projects

--- src/components/Header/HeaderStyle.js
+++ src/components/Header/HeaderStyle.js
@@ -4,14 +4,19 @@ export const HeaderContent = styled.div`
 color: black;
 margin-right: 60em;
 margin-top: -20em;
+
 `;
 
 export const Img = styled.img`
   display: flex;
   margin-top: -2rem;
   height: 5rem;
-  width: 5rem;
-  border: solid black 3px;
+  width: 8rem;
+  transition: transform 0.3s ease; // Adiciona transição suave
+  &:hover {
+    transform: rotate(360deg); // Gira o logo ao passar o mouse
+  }
+  
 `;
 
 export const Nav = styled.nav`
@@ -22,7 +27,7 @@ export const Nav = styled.nav`
 export const Ul = styled.ul`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-between;
   list-style: none; // Remove os marcadores padrão de lista
   padding: 0; // Remove o padding padrão
 `;
@@ -34,6 +39,10 @@ export const Li = styled.li`
 
 export const LinkNav = styled.a`
   color: black;
+  transition: color 0.3s ease;
+  &:hover {
+    color: #FFD100; // Muda a cor ao passar o mouse
+  }
 `;
 
 //C:\Users\Eric\OneDrive\Área de Trabalho\Projetos\AAAAAAAAAAAAPORTFÓLIO\eric-reis-portfolio\src\components\Header\HeaderStyle.js
--- src/components/HeroSection/HeroSectionStyle.js
+++ src/components/HeroSection/HeroSectionStyle.js
@@ -1,32 +1,40 @@
 import styled from "styled-components";
 
 export const HeroSectionContainer = styled.div`
-  background-image: url("/src/assets/img/background-hero/draw-shade.jpg"); // Substitua com sua imagem
-  color: black;
+  
+  color: #202020; // Quase preto
   text-align: center;
   padding: 1rem 2rem;
   margin-top: 1em;
   width: 86rem;
+  /* background-color: #D6D6D6;  */
 `;
 
 export const HeroTitle = styled.h1`
-  font-size: 2.5rem;
-  margin-bottom: 2rem;
-  color: black;
+ font-size: 3rem; // Tamanho maior para mais destaque
+  margin-bottom: 1rem;
+  color: #202020;
+  font-weight: bold; // Negrito para o título
 `;
 
 export const HeroSubtitle = styled.p`
-  font-size: 1.5rem;
-  margin-bottom: 40px;
+font-size: 1.8rem; // Ligeiramente maior para melhor legibilidade
+  margin-bottom: 2rem;
+  color: #333533; // Um tom mais claro para contraste
 `;
 
 export const HeroButton = styled.a`
-  background-color: #0066ff;
-  color: #fff;
-  padding: 10px 30px;
+ background-color: #FFEE32;
+  color: #202020;
+  padding: 12px 35px; // Um pouco maior para destacar
   text-decoration: none;
   font-weight: bold;
   border-radius: 5px;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+  &:hover {
+    background-color: #FFD100;
+    transform: scale(1.05); // Efeito de zoom ao passar o mouse
+  }
 `;
 
 
